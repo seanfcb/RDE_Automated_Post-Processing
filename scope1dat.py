e684c2b224f85a8ff7628b50d145a86ea5a7a57f
seanfcb <sean.connolly.boutin@gmail.com>
2023-04-24 15:12:38 -0400

    Scope data processing script changes -Comments now appear more legibly -Fixed issue where pressure data was still saving to csv with the unit line. -Repeated the entire process for scope2dat.py

--- scope1dat.py
+++ scope1dat.py
@@ -5,15 +5,9 @@ import csv
 import sys
 import os
 
-# def v_to_psi(CH,psi):
-# 	i = 0
-# 	for j in CH:
-# 		j = (j-0.5)/4*psi
-# 		CH[i] = j
-# 		i=i+1
-# 	return CH
+########## User-defined functions #############
 
-def v_to_psi(CH, psi):
+def v_to_psi(CH, psi):  
     for i, x in enumerate(CH[1:], start=1):
         CH[i] = (float(x) - 0.5) / 4 * psi
     return CH
@@ -26,12 +20,22 @@ def time_array(time,START,dt):
 			time[i] = time[i-1]+dt
 	return time
  
-# reading CSV file
+
+####################################################################################################
+#                                           Reading CSV file                                       #
+####################################################################################################
+
 
 shot_num = input("Enter shot number to post-process: ")
 filename = "shot"+ shot_num +"scp1_raw.csv"
 data = read_csv(filename)
 
+
+
+
+####################################################################################################
+#                                           Creating arrays                                        #
+####################################################################################################
 
 #creating arrays
 time1 = data['X'].tolist()
@@ -39,32 +43,44 @@ CH1   = data['CH1'].tolist()
 CH2   = data['CH2'].tolist()
 CH3   = data['CH3'].tolist()
 CH4   = data['CH4'].tolist()
-#START = data['START'].tolist()
 START = data.filter(regex='(?i)^start$', axis=1).squeeze().tolist()
 START = START[0]
-#dt    = data['INCREMENT'].tolist()
 dt    = data.filter(regex='(?i)^increment$', axis=1).squeeze().tolist()
 dt    = dt[0]
 
 #Creating time array
 time1 = time_array(time1,START,dt)
+data['X'] = time1
+
+####################################################################################################
+#                                        Reformatting data frame                                   #
+####################################################################################################
+
 
 #Dropping the first row below headers (units), and the last two columns and saving modified .csv
 
 data = data.drop(index=0) #dropping the units row
 data = data.iloc[:, :-2] #Dropping the last two columns
 data = data.reset_index(drop=True)
-data.to_csv("shot"+ shot_num +"scp1.csv",index=False)
+data.to_csv("shot"+ shot_num +"scp1_volts.csv",index=False)
 
+####################################################################################################
+#                                        Converting voltage to PSI                                 #
+####################################################################################################
 
-#Voltage to PSI
 CH1 = v_to_psi(CH1,5000) #Blue bottle
 CH2 = v_to_psi(CH2,5000) #Red bottle
 CH3 = v_to_psi(CH3,1000) #Blue line, pre-choke
 
+####################################################################################################
+#                                       Saving pressure data to CSV                                #
+####################################################################################################
+
+
 #Creating array for new csv file
 scope1 = DataFrame({"time1":time1,"BlueBottle":CH1,"RedBottle":CH2,"BluePre":CH3,"TRIG":CH4})
-
+scope1 = scope1.drop(index=0) #dropping the units row
+scope1 = scope1.reset_index(drop=True)
 #Saving to .csv
 scope1.to_csv("scope1shot"+shot_num+"_pressure.csv",index=False)
 
--- scope2dat.py
+++ scope2dat.py
@@ -1,45 +1,85 @@
 # importing module
 from pandas import *
-import matplotlib.pyplot as plt
+import numpy as np
+import csv
+import sys
+import os
 
-def v_to_psi(CH,psi):
-	i = 0
-	for j in CH:
-		j = (j-0.5)/4*psi
-		CH[i] = j
-		i=i+1
-	return CH
+########## User-defined functions #############
+
+def v_to_psi(CH, psi):  
+    for i, x in enumerate(CH[1:], start=1):
+        CH[i] = (float(x) - 0.5) / 4 * psi
+    return CH
+
 def time_array(time,START,dt):
-	for i in time:
-		if i == 0:
+	for i in range(1,len(time)):
+		if i == 1:
 			time[i]=START
 		else:
 			time[i] = time[i-1]+dt
 	return time
  
-# reading CSV file
-data = read_csv("shot122scp2.csv")
+
+####################################################################################################
+#                                           Reading CSV file                                       #
+####################################################################################################
+
+
+shot_num = input("Enter shot number to post-process: ")
+filename = "shot"+ shot_num +"scp2_raw.csv"
+data = read_csv(filename)
+
+
+
+
+####################################################################################################
+#                                           Creating arrays                                        #
+####################################################################################################
 
 #creating arrays
 time2  = data['X'].tolist()
 CH1   = data['CH1'].tolist()
 CH2   = data['CH2'].tolist()
 CH3   = data['CH3'].tolist()
-START = data['START'].tolist()
+START = data.filter(regex='(?i)^start$', axis=1).squeeze().tolist()
 START = START[0]
-dt    = data['INCREMENT'].tolist()
+dt    = data.filter(regex='(?i)^increment$', axis=1).squeeze().tolist()
 dt    = dt[0]
 
 #Creating time array
 time2 = time_array(time2,START,dt)
+data['X'] = time2
 
-#Voltage to PSI
+####################################################################################################
+#                                        Reformatting data frame                                   #
+####################################################################################################
+
+
+#Dropping the first row below headers (units), and the last two columns and saving modified .csv
+
+data = data.drop(index=0) #dropping the units row
+data = data.iloc[:, :-2] #Dropping the last two columns
+data = data.reset_index(drop=True)
+data.to_csv("shot"+ shot_num +"scp2_volts.csv",index=False)
+
+####################################################################################################
+#                                        Converting voltage to PSI                                 #
+####################################################################################################
+
 CH1 = v_to_psi(CH1,667) #Blue line, post-choke
 CH2 = v_to_psi(CH2,1000) #Red line, pre-choke
 
+####################################################################################################
+#                                       Saving pressure data to CSV                                #
+####################################################################################################
+
+
 #Creating array for new csv file
 scope2 = DataFrame({"time2":time2,"BluePost":CH1,"RedPre":CH2,"TRIG":CH3})
+scope2 = scope2.drop(index=0) #dropping the units row
+scope2 = scope2.reset_index(drop=True)
 
 #Saving to .csv
-scope2.to_csv("scope2.csv",index=False)
+scope2.to_csv("scope2shot"+shot_num+"_pressure.csv",index=False)
 
